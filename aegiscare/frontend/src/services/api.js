eb6270e73741a52e76974054c9a863e84ebb9595
bkrgill <abubakargill326@gmail.com>
2026-06-16 01:14:43 +0500

    Update frontend API URL for production

--- aegiscare/frontend/src/services/api.js
+++ aegiscare/frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://aegiscare-fyp.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
